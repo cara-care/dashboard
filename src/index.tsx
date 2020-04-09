3d2d7e33a41188ff62101b17e11ce086502478d5
Jan Czizikow <jan@gohidoc.com>
2020-04-09 13:55:17 +0200

    add console log message 🥚

--- src/index.tsx
+++ src/index.tsx
@@ -38,4 +38,19 @@ function runApp() {
   // unregister() to register() below. Note this comes with some pitfalls.
   // Learn more about service workers: https://bit.ly/CRA-PWA
   serviceWorker.unregister();
+  if (process.env.NODE_ENV === 'production') {
+    console.log(
+      `%c
+
+_____ _____ _____ _____    _____ _____ _____ _____  \n\
+|     |  _  | __  |  _  |  |     |  _  | __  |   __| \n\
+|   --|     |    -|     |  |   --|     |    -|   __| \n\
+|_____|__|__|__|__|__|__|  |_____|__|__|__|__|_____| \n\
+
+🤖Something missing?
+See https://github.com/cara-care/dashboard - pull requests are welcome!
+`,
+      'font-family:SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;color:#00b3a5;font-size:12px;'
+    );
+  }
 }
