ea4873e471687623957400390c457ffc4160c88d
Jan Czizikow <jan@gohidoc.com>
2020-05-06 16:03:21 +0200

    test questionnairesReducer and questionnairesSagas

---
+++ src/questionnaires/testUtils.ts
@@ -0,0 +1,13 @@
+export const mockSubmission = {
+  id: 1,
+  name: 'submission',
+  started: new Date(),
+  completed: new Date(),
+};
+
+export const mockResult = {
+  count: 1,
+  previous: null,
+  next: null,
+  results: [mockSubmission],
+};
