34508e5f01c3eccf50c1d3ad95ad02e39f0e1b75
Jan Czizikow <jan@gohidoc.com>
2020-06-19 14:51:25 +0200

    refactor

--- cypress/e2e/dashboard.js
+++ cypress/e2e/dashboard.js
@@ -1,62 +1,27 @@
-import _ from 'lodash';
-import {
-  EXPORT_TOKEN_INVALID,
-  TRACKING_OVERVIEW_DATEPICKER,
-  LANGUAGE_MENU_BUTTON,
-} from '../../src/utils/test-helpers';
+import { EXPORT_TOKEN_INVALID } from '../../src/utils/test-helpers';
 
 describe('dashboard', () => {
+  const EXPORT_TOKEN = 'pPEbU07DVOpWNBRMMuA0fyMWgwzppHfEZ';
+
   it('shows an error if token is invalid', () => {
     cy.visit('/export/invalid-token');
     cy.findByTestId(EXPORT_TOKEN_INVALID).should('be.visible');
   });
 
   it('shows data in tracking overview', () => {
-    cy.visit('/export/pPEbU07DVOpWNBRMMuA0fyMWgwzppHfEZ');
-    // open datepicker
-    cy.findByTestId(TRACKING_OVERVIEW_DATEPICKER).click();
-    // change year to 2020
-    cy.findByText(new Date().getFullYear().toString()).click();
-    cy.get('.MuiPickersYearSelection-container').findByText(/2020/i).click();
-    // change month to June
-    const month = new Date().getMonth() + 1;
-    const TARGET_MONTH = 6;
-    if (month !== TARGET_MONTH) {
-      if (month < TARGET_MONTH) {
-        // click ">" button until it's June
-        _.times(TARGET_MONTH - month, async () => {
-          cy.get(
-            '.MuiPickersCalendarHeader-switchHeader > :nth-child(3) > .MuiIconButton-label > .MuiSvgIcon-root'
-          )
-            .first()
-            .click();
-        });
-      } else {
-        // click "<" button until it's June
-        _.times(month - TARGET_MONTH, async () => {
-          cy.get(
-            '.MuiPickersCalendarHeader-switchHeader > :nth-child(1) > .MuiIconButton-label > .MuiSvgIcon-root'
-          )
-            .first()
-            .click();
-        });
-      }
-    }
-    // change date to 12th
-    cy.findByText(/12/i).click();
+    const LANGUAGE_CODE = 'de';
+    cy.visit(`/export/${EXPORT_TOKEN}`);
+    cy.openTrackingOverviewDatePicker();
+    cy.setDatePickerDateTo({ year: 2020, month: 6, day: 12 });
     cy.get('.MuiChip-label').should('have.text', 'Beer');
-
-    // change language to German
-    cy.findByTestId(LANGUAGE_MENU_BUTTON).click();
-    cy.findByText(/deutsch/i).click();
+    cy.changeLanguage(LANGUAGE_CODE);
     cy.get('.MuiChip-label').should('have.text', 'Bier');
-    // make sure the locale is persisted in localStorage
-
+    // make sure the locale is persisted in localStorage after changing the language
     // TODO: Rewrite test in TypeScript and import the constant for this
     // the time should match the timeout in `src/index.tsx`
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(3500).then(() => {
-      expect(localStorage.getItem('locale')).to.eq('de');
+      expect(localStorage.getItem('locale')).to.eq(LANGUAGE_CODE);
     });
   });
 });
