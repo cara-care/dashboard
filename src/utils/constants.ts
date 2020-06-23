54b49b81270d3d7aed0d770aa6f67039d9b58cbb
Jan Czizikow <jan@gohidoc.com>
2020-06-23 14:26:09 +0200

    remove unsed const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,3 @@
-export const BACKEND_HOST =
-  process.env.NODE_ENV === 'production'
-    ? 'https://backend.gohidoc.com'
-    : 'https://backend-staging.gohidoc.com';
 export const INTERCOM_APP_ID = process.env.REACT_APP_INTERCOM_APP_ID;
 export const LOCALES = [
   { code: 'en', locale: 'English' },
