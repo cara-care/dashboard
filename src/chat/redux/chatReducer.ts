cb10c9669e28aca3d271fc9d7c60bb0a80f2a6e9
pavelsof <mail@pavelsof.com>
2021-09-16 14:54:11 +0200

    Chat: removed the now obsolete Redux stuff.

--- src/chat/redux/chatReducer.ts
+++ src/chat/redux/chatReducer.ts
@@ -1,7 +1,6 @@
 import { Reducer } from 'redux';
 import { ChatActions, ChatActionTypes } from './chatActions';
 import { RootState } from '../../utils/store';
-import { uniqBy } from 'lodash';
 import { findLastContact } from './utils';
 import {
   ChatUser,
@@ -67,88 +66,7 @@ export const chatReducer: Reducer<ChatState, ChatActions> = (
   action: ChatActions
 ) => {
   switch (action.type) {
-    // Current User
-    case ChatActionTypes.SET_CURRENT_USER_LOADING:
-      return {
-        ...state,
-        loadingCurrentUser: action.payload,
-      };
-    case ChatActionTypes.SET_CURRENT_CHAT_USER:
-      return {
-        ...state,
-        currentChatUser: action.user,
-        selectedChatAssignment: action.user?.inbox ?? '_',
-      };
-    case ChatActionTypes.CLEAR_CURRENT_CHAT_USER:
-      return {
-        ...state,
-        currentChatUser: null,
-      };
-
-    // Chat Messages
-    case ChatActionTypes.ADD_CHAT_MESSAGE:
-      const isCurrentChat =
-        action.chatMessage.room === state.currentChatUser?.username;
-      if (!isCurrentChat) return { ...state };
-      return {
-        ...state,
-        chatMessages: [action.chatMessage, ...state.chatMessages],
-        scrollToChatBottom: true,
-      };
-    case ChatActionTypes.SET_CHAT_MESSAGES:
-      const newChatMessages = uniqBy(
-        [...state.chatMessages, ...action.chatMessages],
-        'id'
-      );
-      return {
-        ...state,
-        chatMessages: newChatMessages,
-      };
-    case ChatActionTypes.CLEAR_CHAT_MESSAGES:
-      return {
-        ...state,
-        chatMessages: [],
-      };
-
-    // Chat Rooms
-    case ChatActionTypes.ADD_CHAT_ROOM:
-      return {
-        ...state,
-        chatRooms: [action.chatRoom, ...state.chatRooms],
-      };
-    case ChatActionTypes.SET_CHAT_ROOMS:
-      const newChatRooms = action.isNewRoom
-        ? uniqBy(action.chatRooms, 'patient.id')
-        : uniqBy([...state.chatRooms, ...action.chatRooms], 'patient.id');
-      return {
-        ...state,
-        chatRooms: newChatRooms,
-      };
-    case ChatActionTypes.CLEAR_CHAT_ROOMS:
-      return {
-        ...state,
-        chatRooms: [],
-      };
-    case ChatActionTypes.ADD_NEW_MESSAGE_TO_CHAT_ROOM:
-      const updatedRooms = [...state.chatRooms];
-
-      const [newestRoom] = updatedRooms.splice(action.index, 1);
-      newestRoom.lastMessage = action.message;
-      updatedRooms.unshift(newestRoom);
-      // TODO: add unread mark
-      return {
-        ...state,
-        chatRooms: updatedRooms,
-      };
-
-    // CHAT CONVERSATIONS
-    case ChatActionTypes.SET_CHAT_CONVERSATIONS:
-      return {
-        ...state,
-        chatConversations: [...action.chatConversations],
-      };
-
-    // CHAT NOTES
+    // Notes
     case ChatActionTypes.SET_CHATUSER_NOTES:
       return {
         ...state,
@@ -189,24 +107,10 @@ export const chatReducer: Reducer<ChatState, ChatActions> = (
       };
 
     // Other
-    case ChatActionTypes.SET_CHAT_ROOMS_SLUG:
-      const conversation = state.chatConversations.find(
-        (el) => el.name === action.payload
-      );
-      return {
-        ...state,
-        selectedChatConversation:
-          conversation ?? chatInitialState.selectedChatConversation,
-      };
     case ChatActionTypes.SET_SELECTED_ASSIGNMENT:
       return {
         ...state,
         selectedChatAssignment: action.payload,
-      };
-    case ChatActionTypes.SET_SCROLL_TO_BOTTOM:
-      return {
-        ...state,
-        scrollToChatBottom: action.payload,
       };
 
     // Kabelwerk
