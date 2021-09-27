50ddb4d0034d2458606a9066df479ca21bc88ff4
pavelsof <pavel@cara.care>
2021-09-27 23:13:28 +0200

    Environment (#65)
    * Towards putting all config in env vars.
    
    * Removed the unnecessary constants.
    
    * Removed the now unnecessary deploy.sh script.
    
    * Removed the nutri navigation in the EU dashboard.
    
    And removed the chat from the nutri navigation in the non-EU one.
    
    * Fixed the broken test.

--- src/chat/pages/Inbox.tsx
+++ src/chat/pages/Inbox.tsx
@@ -12,7 +12,6 @@ import ChatHeader from '../components/chatHeader/ChatHeader';
 import InboxSidebar from '../components/inboxSidebar/InboxSidebar';
 import { getChatAuthorizationToken } from '../../utils/api';
 import { INBOXES } from '../inboxes';
-import { KABELWERK_URL } from '../../utils/constants';
 import { CHAT_WRAPPER } from '../../utils/test-helpers';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexWrap: 'nowrap',
     width: '100vw',
-    height: `calc(100vh - 68px - 64px)`, // 100vh - nutri navigation height - appbar height
+    height: `calc(100vh - 10px - 64px)`, // 100vh - nutri navigation height - appbar height
   },
   resize: {
     display: 'flex',
@@ -57,7 +56,7 @@ export default function Inbox() {
     if (!Kabelwerk.isConnected()) {
       getChatAuthorizationToken().then((res) => {
         Kabelwerk.config({
-          url: KABELWERK_URL,
+          url: process.env.REACT_APP_KABELWERK_URL,
           token: res.data.token,
           refreshToken: () => {
             return getChatAuthorizationToken().then((res) => res.data.token);
