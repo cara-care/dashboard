7eff0e12a324dcd94bfc6762cef4ae4e48245d25
stelaseldano <stella.plamenova@gmail.com>
2021-10-29 15:15:06 +0200

    remove unused var

--- src/chat/components/chatHeader/AssignTeammate.tsx
+++ src/chat/components/chatHeader/AssignTeammate.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import { Paper, Theme, Typography, useTheme } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Avatar, Paper, Theme, Typography, useTheme } from '@material-ui/core';
-import styled from 'styled-components';
+import React from 'react';
 import { useIntl } from 'react-intl';
+import styled from 'styled-components';
 import useKabelwerk from '../../hooks/useKabelwerk';
 import useNotification from '../../hooks/useNotification';
 
