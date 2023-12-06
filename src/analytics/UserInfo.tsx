811081cd84771c2be1302bad300a6e15dad1a6ba
Asad Manzoor <asad33manzoor@gmail.com>
2023-12-06 21:23:52 +0500

    Analytics Dashboard: User Actions Feature

--- src/analytics/UserInfo.tsx
+++ src/analytics/UserInfo.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
@@ -58,6 +59,11 @@ const useStyles = makeStyles((theme) => ({
   infoValue: {
     textAlign: 'left',
   },
+  inputBase: {
+    marginLeft: '10px',
+    ml: 10,
+    flex: 1,
+  },
 }));
 
 interface UserInfoProps {
@@ -67,6 +73,10 @@ interface UserInfoProps {
 const UserInfo = function ({ userData }: UserInfoProps) {
   const classes = useStyles();
   const [selectedQuestionnaire, setSelectedQuestionnaire] = useState('');
+  const [action, setAction] = useState('');
+  const [collection, setCollection] = useState('');
+  const [module, setModule] = useState('');
+  const [questionnaire, setQuestionnaire] = useState('');
 
   useEffect(() => {
     setSelectedQuestionnaire('');
@@ -78,6 +88,10 @@ const UserInfo = function ({ userData }: UserInfoProps) {
   }) => {
     e.preventDefault();
     setSelectedQuestionnaire(e.target.value);
+  };
+
+  const handleUserAction = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
   };
 
   return (
@@ -172,13 +186,16 @@ const UserInfo = function ({ userData }: UserInfoProps) {
           <div>
             {userData['subscriptions'] &&
               userData['subscriptions'].map(
-                (subscription: {
-                  code: any;
-                  start: any;
-                  end: any;
-                  is_code_redeemed: any;
-                }) => (
-                  <div className={classes.infoContainer}>
+                (
+                  subscription: {
+                    code: any;
+                    start: any;
+                    end: any;
+                    is_code_redeemed: any;
+                  },
+                  index: string | number | undefined
+                ) => (
+                  <div className={classes.infoContainer} key={index}>
                     <div className={classes.info}>
                       <span className={classes.infoLabel}>Code:</span>
                       <span className={classes.infoValue}>
@@ -257,6 +274,174 @@ const UserInfo = function ({ userData }: UserInfoProps) {
       </div>
       <br />
       <br />
+      <div>
+        <strong>User Action: </strong>
+        <select
+          onChange={(e) => {
+            setAction(e.target.value);
+          }}
+          value={action}
+          className={classes.select}
+        >
+          <option value="" className={classes.selectOption}>
+            Select User Action
+          </option>
+          <option
+            key="add_module"
+            value="add_module"
+            className={classes.selectOption}
+          >
+            Add Module
+          </option>
+          <option
+            key="remove_module"
+            value="remove_module"
+            className={classes.selectOption}
+          >
+            Remove Module
+          </option>
+          <option
+            key="complete_questionnaire"
+            value="complete_questionnaire"
+            className={classes.selectOption}
+          >
+            Complete Questionnaire
+          </option>
+          <option
+            key="update_recipe_collection"
+            value="update_recipe_collection"
+            className={classes.selectOption}
+          >
+            Update Recipe Collection
+          </option>
+        </select>
+
+        {action === 'complete_questionnaire' ? (
+          <select
+            onChange={(e) => {
+              setQuestionnaire(e.target.value);
+            }}
+            value={questionnaire}
+            className={classes.select}
+          >
+            <option value="" className={classes.selectOption}>
+              Select Questionnaire
+            </option>
+            {Object.entries(userData['questionnaires']).map(([slug]) => (
+              <option key={slug} value={slug} className={classes.selectOption}>
+                {slug}
+              </option>
+            ))}
+          </select>
+        ) : (
+          ''
+        )}
+
+        {action === 'update_recipe_collection' ? (
+          <select
+            onChange={(e) => {
+              setCollection(e.target.value);
+            }}
+            value={collection}
+            className={classes.select}
+          >
+            <option value="" className={classes.selectOption}>
+              Select Recipe Collection
+            </option>
+            <option key="1" value="1" className={classes.selectOption}>
+              Gut-Friendly
+            </option>
+            <option key="2" value="2" className={classes.selectOption}>
+              HB-Friendly
+            </option>
+            <option key="3" value="3" className={classes.selectOption}>
+              Low-FODMAP
+            </option>
+            <option key="4" value="4" className={classes.selectOption}>
+              Mediterranean-Diet
+            </option>
+            <option key="5" value="5" className={classes.selectOption}>
+              IBD-Flareup
+            </option>
+            <option key="6" value="6" className={classes.selectOption}>
+              HB-Weightloss
+            </option>
+          </select>
+        ) : (
+          ''
+        )}
+
+        {action === 'add_module' || action === 'remove_module' ? (
+          <select
+            onChange={(e) => {
+              setModule(e.target.value);
+            }}
+            value={module}
+            className={classes.select}
+          >
+            <option value="" className={classes.selectOption}>
+              Select Module
+            </option>
+            <option key="2" value="2" className={classes.selectOption}>
+              hypno
+            </option>
+            <option key="3" value="3" className={classes.selectOption}>
+              cbt
+            </option>
+            <option key="1" value="1" className={classes.selectOption}>
+              low-fodmap
+            </option>
+            <option key="21" value="21" className={classes.selectOption}>
+              HB-weight-loss
+            </option>
+            <option key="22" value="22" className={classes.selectOption}>
+              HB-weight-loss-hypnosis
+            </option>
+            <option key="14" value="14" className={classes.selectOption}>
+              HB-Nutrition
+            </option>
+            <option key="18" value="18" className={classes.selectOption}>
+              HB-Hypnosis-FD
+            </option>
+            <option key="17" value="17" className={classes.selectOption}>
+              HB-Hypnosis-GERD
+            </option>
+            <option key="13" value="13" className={classes.selectOption}>
+              HB-CBT
+            </option>
+            <option key="15" value="15" className={classes.selectOption}>
+              HB-Relaxation
+            </option>
+            <option key="19" value="19" className={classes.selectOption}>
+              IBD-PE
+            </option>
+            <option key="7" value="7" className={classes.selectOption}>
+              IBD-Low-FODMAP
+            </option>
+            <option key="11" value="11" className={classes.selectOption}>
+              IBD-Relaxation
+            </option>
+            <option key="10" value="10" className={classes.selectOption}>
+              IBD-CBT
+            </option>
+            <option key="8" value="8" className={classes.selectOption}>
+              IBD-MedDiet
+            </option>
+          </select>
+        ) : (
+          ''
+        )}
+
+        <Button
+          color="primary"
+          type="button"
+          variant="contained"
+          onClick={handleUserAction}
+        >
+          Submit
+        </Button>
+      </div>
+      <br />
       {userData['questionnaires'] && (
         <div>
           <strong>Questionnaires: </strong>
@@ -275,21 +460,27 @@ const UserInfo = function ({ userData }: UserInfoProps) {
       <br />
       {userData['user_answers'] && (
         <div>
-          <strong>User Answers: </strong>
-          <select
-            onChange={handleQuestionnaire}
-            value={selectedQuestionnaire}
-            className={classes.select}
-          >
-            <option value="" className={classes.selectOption}>
-              Select Questionnaire
-            </option>
-            {Object.entries(userData['questionnaires']).map(([slug]) => (
-              <option key={slug} value={slug} className={classes.selectOption}>
-                {slug}
+          <div>
+            <strong>User Answers: </strong>
+            <select
+              onChange={handleQuestionnaire}
+              value={selectedQuestionnaire}
+              className={classes.select}
+            >
+              <option value="" className={classes.selectOption}>
+                Select Questionnaire
               </option>
-            ))}
-          </select>
+              {Object.entries(userData['questionnaires']).map(([slug]) => (
+                <option
+                  key={slug}
+                  value={slug}
+                  className={classes.selectOption}
+                >
+                  {slug}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       )}
       {selectedQuestionnaire && (
